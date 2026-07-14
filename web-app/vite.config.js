df312a36401035989e35f34508d4597254ecb006
Bhavya Singh <bhavya1singh1@gmail.com>
2026-07-14 23:58:29 +0530

    Correcte wrong paths

--- web-app/vite.config.js
+++ web-app/vite.config.js
@@ -1,1 +1,15 @@
-import { defineConfig } from "vite"; export default defineConfig({ base: "./" });
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  base: "./",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        bihar: resolve(__dirname, 'bihar.html'),
+        maharashtra: resolve(__dirname, 'maharashtra.html')
+      }
+    }
+  }
+});
